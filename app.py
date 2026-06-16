94dc65c766488ef06756bab22082edf206807be8
viscorquality-ux <viscorquality@gmail.com>
2026-06-16 12:34:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-from flask_sqlalchemy import SQLAlchemy, text
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 from datetime import datetime, timedelta
 import pytz
 
