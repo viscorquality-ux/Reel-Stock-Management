ed1cf090780e08f98afcb53c2de18ed68e43e324
viscorquality-ux <viscorquality@gmail.com>
2026-07-02 18:05:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -953,11 +953,11 @@ def calculate_reel_size(width, height, position, ply):
     """
     if position.lower() == 'internal':
         if ply == 3:
-            base_1_ups = ((width + 4) / 2) + (height + 3) + 2
+            base_1_ups = (width + 4) + (height + 3) + 2
         elif ply == 5:
-            base_1_ups = ((width + 8) / 2) + (height + 3) + 2
+            base_1_ups = (width + 8) + (height + 3) + 2
     else: # External
-        base_1_ups = (width / 2) + height + 2
+        base_1_ups = (width) + height + 2
 
     # Available Reel Sizes (75cm සිට 150cm දක්වා 5න් 5ට)
     standard_sizes = list(range(75, 155, 5))
